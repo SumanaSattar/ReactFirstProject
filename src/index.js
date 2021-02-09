e5e23d10e06d3643f9b29c8bdd8d9f475d9af866
Sumana Sattar <50830293+SumanaSattar@users.noreply.github.com>
2021-02-09 16:55:55 +0100

    card component is done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
-
+import 'tachyons';
+import {robots} from './robots';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Card name={robots[0].name} email={robots[0].email}/>
+    <Card name={robots[1].name} email={robots[1].email}/>
+    <Card name={robots[2].name} email={robots[2].email}/>
+    <Card name={robots[3].name} email={robots[3].email}/>
+    <Card name={robots[4].name} email={robots[4].email}/>
+    <Card name={robots[5].name} email={robots[5].email}/>
+    <Card name={robots[6].name} email={robots[6].email}/>
+    <Card name={robots[7].name} email={robots[7].email}/>
+    <Card name={robots[8].name} email={robots[8].email}/>
+    <Card name={robots[9].name} email={robots[9].email}/>
+
+
   </React.StrictMode>,
   document.getElementById('root')
 );
